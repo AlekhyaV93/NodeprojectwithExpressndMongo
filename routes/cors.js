4fe67e6f4664415767561ee95a593eb961745882
Alekhya <vinnakotaalekhya@gmail.com>
2020-08-27 23:46:52 -0500

    cors file modified

--- routes/cors.js
+++ routes/cors.js
@@ -12,4 +12,4 @@ const corsOptionsDelegate = (req, callback) => {
     callback(null, corsOptions);
 };
 exports.cors = cors();//when we use cors in an end-point it means we accepts requests from all origins
-exports.corsWithOptions = cors(corsOptionsDelegate);//whereas if we use corsWithOptions in an endpoint we explicitly configured to accepts requests from origins mentioned in whitelist array
+exports.corsWithOptions = cors(corsOptionsDelegate);//whereas if we use corsWithOptions in an endpoint we explicitly configured to accepts requests from origins mentioned in 'whitelist' array
